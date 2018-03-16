4a9c6e221819fdd24f07b9132246323c0a0d410d
hazalmulgan <hzlmlgn@gmail.com>
2018-03-16 21:18:30 +0200

    experiencepage

--- app/app.module.js
+++ app/app.module.js
@@ -1,5 +1,5 @@
 'use strict';
 
-angular.module('archApp',[
-  'projectSlider'        
+angular.module('archApp', [
+  'projectSlider' , 'experiencePage'       
 ]);
---
+++ app/experience-page/experience-page.component.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.
+  module('experiencePage').
+    component('experiencePage', {
+      templateUrl: 'experience-page/experience-page.template.html',
+      controller: function experiencePageController() {
+      this.architects = {
+          "0": {
+              'name':"Murat Danışman",
+             'info': [
+              {'imgURL': 'images/exp1.png',
+              'projectName': 'bechtel-enka',
+              'projectDetail':'Muscat havalimanı'},
+                 
+              {'imgURL': 'images/exp1.png',
+              'projectName': 'EAA',
+              'projectDetail':'ulus-savoy'}
+          ]
+                 
+      },
+          
+             "1": {
+              'name':"Gamze MULGAN",
+             'info': [
+              {'imgURL': 'images/exp1.png',
+              'projectName': 'İGA',
+              'projectDetail':'Proje kordinasyon'}
+          ]
+                 
+      }
+      }
+               console.log(this.architects); 
+      }
+      });
+          
