158f748604521e5e3713ce9f9da1e41ec274b4cd
hazalmulgan <hzlmlgn@gmail.com>
2018-03-21 16:40:26 +0200

    main page has added

--- app/app.module.js
+++ app/app.module.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('archApp', [
-  'projectSlider' , 'experiencePage'       
+  'projectSlider' , 'experiencePage'  ,'mainPage'     
 ]);
---
+++ app/main-page/main-page.component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.
+  module('mainPage').
+    component('mainPage', {
+      templateUrl: 'main-page/main-page.template.html',
+      controller: function mainPageController() {
+   
+      }
+      });
+          
